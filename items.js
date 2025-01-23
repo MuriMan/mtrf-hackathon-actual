6992c14fc1feaffb8e1d3aae44fe612f74889329
Aritra Ghosh <aritraghosh5002@gmail.com>
2025-01-23 04:12:52 -0500

    almost done with everything

--- items.js
+++ items.js
@@ -1,25 +1,69 @@
-// the list of items
-const items = [
-	new Item(0, "Scissors", 2, []),
-	new Item(1, "Scissors", 3, []),
-	new Item(2, "Scissors", 4, []),
-	new Item(3, "Glue", 5, []),
-	new Item(4, "Glue", 6, []),
-];
-
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 
 items.forEach(obj => {
-	const item = document.createElement('div');
-	item.classList.add('item');
+	const item = document.createElement("div");
+	item.classList.add("item");
 
-
-	//assalamualaikum abrar
-	// aritra beautify this shit.
-	// also put in a link to the borrowpage for each item
+	// display description here as well aritra
 	item.innerHTML = `<strong class = "item-name">${obj.name}</strong>
 		<br> <strong class = "item-amount">Amount Available:</strong> ${(obj.amount - obj.borrowers.length)} 
-		<br></br> <a href = "./confirm_borrow.html" class = "borrow-button">Borrow</a>`;
+		<br></br>  <button onclick="studentBorrow(event, ${obj.id})" class = "borrow-button">Borrow</button>`;
 
 	container.appendChild(item);
 });
+
+function showItems() {
+	container.textContent = "";
+
+	items.forEach(obj => {
+		const item = document.createElement("div");
+		item.classList.add("item");
+
+		// display description here as well aritra
+		item.innerHTML = `<strong class = "item-name">${obj.name}</strong> <br> <strong class = "item-amount">Amount Available:</strong> ${(obj.amount - obj.borrowers.length)} 
+			<br></br><button onclick="studentBorrow(event, ${obj.id})">borrow</button>`;
+
+		container.appendChild(item);
+	});
+
+}
+
+// needs significant work, will work.
+function filterArray() {
+	const query = document.getElementById("searchItems").value.toLowerCase();
+	const results = items.filter(item => item.name.toLowerCase().includes(query));
+
+	displayResults(results);
+}
+
+function displayResults(results) {
+	const resultsList = document.getElementById("container");
+
+	item.innerHTML = ""
+	results.forEach(obj => {
+		const item = document.createElement("div");
+		item.classList.add("item");
+
+		item.innerHTML = `<strong class = "item-name">${obj.name}</strong>
+			<br> <strong class = "item-amount">Amount Available:</strong> ${(obj.amount - obj.borrowers.length)} <br></br> <button onclick="studentBorrow(event, ${obj.id})">borrow</button>`;
+
+		resultsList.appendChild(item);
+	});
+
+	if (results.length === 0) {
+		const li = document.createElement("li");
+		li.innerHTML = "No results found";
+		resultsList.appendChild(li);
+	}
+}
+
+function borrow(studentID, itemID) {
+	// id must equal array position
+	if (itemID < items.length) {
+		if (items[itemID].amount > 0) {
+			items[itemID].borrowers.push(studentID); // its an array oopsies.
+		}
+	}
+
+	location.reload();
+}
